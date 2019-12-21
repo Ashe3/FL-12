f3846983a09faf7a62cf36502e8e8357eb600b56
Ashe3 <Korolewsky.anton3@gmail.com>
2019-12-21 19:29:11 +0200

    HW6

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -1,1 +1,19 @@
-// Your code goes here
+const a = Number(prompt('Input a'));
+const b = Number(prompt('Input b'));
+const c = Number(prompt('Input c'));
+
+if (!Number.isInteger(a) || !Number.isInteger(b) || !Number.isInteger(c) || a === 0) {
+  console.log('Invalid input data')
+} else {
+  const discriminant = Math.pow(b, 2) - 4 * a * c;
+  if (discriminant > 0) {
+    const x1 = Math.round((-b + Math.sqrt(discriminant)) / (2 * a));
+    const x2 = Math.round((-b - Math.sqrt(discriminant)) / (2 * a));
+    console.log(`x1 = ${x1} and x2 = ${x2}`);
+  } else if (discriminant === 0) {
+    const x = Math.round(-b / (2 * a));
+    console.log(`x = ${x}`);
+  } else {
+    console.log('no solution');
+  }
+}
--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -1,1 +1,19 @@
-// Your code goes here
+const a = prompt('Input a');
+const b = prompt('Input b');
+const c = prompt('Input c');
+
+if (a === '' || b === '' || c === '' || !a || !b || !c) {
+  alert('input values should be ONLY numbers');
+  console.log('Triangle doesn’t exist')
+} else if (Number(a) <= 0 || Number(b) <= 0 || Number(c) <= 0) {
+  alert('A triangle must have 3 sides with a positive definite length');
+  console.log('Triangle doesn’t exist')
+} else {
+  if (a === b && b === c && c === a) {
+    console.log('Equilateral triangle');
+  } else if (a === b || b === c || c === a) {
+    console.log('Isosceles triangle');
+  } else {
+    console.log('Scalene triangle');
+  }
+}

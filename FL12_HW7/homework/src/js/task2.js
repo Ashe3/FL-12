71da6b0c097c40f5f0a9dd8e19b7fffba1f15191
Anton Shevchenko <korolewsky.anton3@gmail.com>
2019-12-25 15:06:25 +0200

    chg main logic

--- FL12_HW7/homework/src/js/task2.js
+++ FL12_HW7/homework/src/js/task2.js
@@ -1,8 +1,8 @@
 if (confirm('Do you want to play a game?')) {
-  let pool = 0, play = true;
+  let play = true;
   const factorCounter = 4, prizeMultipler = 2, baseAttemps = 3;
   while (play) {
-    let attempts = 3, factor = 8, basePrize = 25;
+    let attempts = 3, factor = 8, basePrize = 25, pool = 0;
     do {
       const number = Math.floor(Math.random() * (factor + 1));
       const prize = basePrize * Math.pow(prizeMultipler, attempts - 1);
@@ -25,7 +25,7 @@ if (confirm('Do you want to play a game?')) {
       attempts -= 1;
     } while (attempts > 0);
     alert(`Thank you for your participation. Your prize is: ${pool}$`);
-    play = confirm('What to play more?');
+    play = confirm('What to play again?');
   }
 } else {
   alert('You did not become a billionaire, but can.');
